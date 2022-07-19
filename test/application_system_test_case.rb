a61472611a3d95623ae71259c5752a2dd17c671d
Tom Hughes <tom@compton.nu>
2022-07-20 00:34:43 +0100

    Switch to using chrome for system tests
    Firefox in Ubuntu 22.04 is now only available as a snap and the
    snap version does not currently include geckodriver so can't be
    used with Selenium for testing.
    
    Chrome in headless mode doesn't support using preferences to set
    the language so we have to modify the environment instead.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 ENV.delete("http_proxy")
 
+ENV["LANG"] = "en.UTF-8"
+
 ActiveSupport.on_load(:action_dispatch_system_test_case) do
   ActionDispatch::SystemTesting::Server.silence_puma = true
 end
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, :using => :headless_firefox do |options|
-    options.add_preference("intl.accept_languages", "en")
-  end
+  driven_by :selenium, :using => :headless_chrome
 end
