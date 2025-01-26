83dde9e78e9ea4eb6a7be3e5cb43bf740082f6b2
Tom Hughes <tom@compton.nu>
2025-01-26 19:06:27 +0000

    Replace uses of var with let or const as appropriate

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -24,7 +24,7 @@
 //= require router
 
 $(document).ready(function () {
-  var map = new L.OSM.Map("map", {
+  const map = new L.OSM.Map("map", {
     zoomControl: false,
     layerControl: false,
     contextmenu: true,
@@ -32,8 +32,6 @@ $(document).ready(function () {
   });
 
   OSM.loadSidebarContent = function (path, callback) {
-    var content_path = path;
-
     map.setSidebarOverlaid(false);
 
     $("#sidebar_loader").show().addClass("delayed-fade-in");
@@ -41,7 +39,7 @@ $(document).ready(function () {
     $("#sidebar_content")
       .empty();
 
-    fetch(content_path, { headers: { "accept": "text/html", "x-requested-with": "XMLHttpRequest" } })
+    fetch(path, { headers: { "accept": "text/html", "x-requested-with": "XMLHttpRequest" } })
       .then(response => {
         $("#flash").empty();
         $("#sidebar_loader").removeClass("delayed-fade-in").hide();
@@ -69,7 +67,7 @@ $(document).ready(function () {
       });
   };
 
-  var params = OSM.mapParams();
+  const params = OSM.mapParams();
 
   map.attributionControl.setPrefix("");
 
@@ -81,19 +79,19 @@ $(document).ready(function () {
     }
   });
 
-  var sidebar = L.OSM.sidebar("#map-ui")
+  const sidebar = L.OSM.sidebar("#map-ui")
     .addTo(map);
 
-  var position = $("html").attr("dir") === "rtl" ? "topleft" : "topright";
+  const position = $("html").attr("dir") === "rtl" ? "topleft" : "topright";
 
   function addControlGroup(controls) {
     for (const control of controls) control.addTo(map);
 
-    var firstContainer = controls[0].getContainer();
+    const firstContainer = controls[0].getContainer();
     $(firstContainer).find(".control-button").first()
       .addClass("control-button-first");
 
-    var lastContainer = controls[controls.length - 1].getContainer();
+    const lastContainer = controls[controls.length - 1].getContainer();
     $(lastContainer).find(".control-button").last()
       .addClass("control-button-last");
   }
@@ -157,7 +155,7 @@ $(document).ready(function () {
 
   $(".leaflet-control .control-button").tooltip({ placement: "left", container: "body" });
 
-  var expiry = new Date();
+  const expiry = new Date();
   expiry.setYear(expiry.getFullYear() + 10);
 
   map.on("moveend baselayerchange overlayadd overlayremove", function () {
@@ -179,11 +177,11 @@ $(document).ready(function () {
     Cookies.set("_osm_welcome", "hide", { secure: true, expires: expiry, path: "/", samesite: "lax" });
   });
 
-  var bannerExpiry = new Date();
+  const bannerExpiry = new Date();
   bannerExpiry.setYear(bannerExpiry.getFullYear() + 1);
 
   $("#banner .btn-close").on("click", function (e) {
-    var cookieId = e.target.id;
+    const cookieId = e.target.id;
     $("#banner").hide();
     e.preventDefault();
     if (cookieId) {
@@ -194,7 +192,7 @@ $(document).ready(function () {
   if (OSM.MATOMO) {
     map.on("baselayerchange overlayadd", function (e) {
       if (e.layer.options) {
-        var goal = OSM.MATOMO.goals[e.layer.options.layerId];
+        const goal = OSM.MATOMO.goals[e.layer.options.layerId];
 
         if (goal) {
           $("body").trigger("matomogoal", goal);
@@ -216,22 +214,22 @@ $(document).ready(function () {
   $("#homeanchor").on("click", function (e) {
     e.preventDefault();
 
-    var data = $(this).data(),
-        center = L.latLng(data.lat, data.lon);
+    const data = $(this).data(),
+          center = L.latLng(data.lat, data.lon);
 
     map.setView(center, data.zoom);
     L.marker(center, { icon: OSM.getUserIcon() }).addTo(map);
   });
 
   function remoteEditHandler(bbox, object) {
-    var remoteEditHost = "http://127.0.0.1:8111",
-        osmHost = location.protocol + "//" + location.host,
-        query = new URLSearchParams({
-          left: bbox.getWest() - 0.0001,
-          top: bbox.getNorth() + 0.0001,
-          right: bbox.getEast() + 0.0001,
-          bottom: bbox.getSouth() - 0.0001
-        });
+    const remoteEditHost = "http://127.0.0.1:8111",
+          osmHost = location.protocol + "//" + location.host,
+          query = new URLSearchParams({
+            left: bbox.getWest() - 0.0001,
+            top: bbox.getNorth() + 0.0001,
+            right: bbox.getEast() + 0.0001,
+            bottom: bbox.getSouth() - 0.0001
+          });
 
     if (object && object.type !== "note") query.set("select", object.type + object.id); // can't select notes
     sendRemoteEditCommand(remoteEditHost + "/load_and_zoom?" + query, function () {
@@ -254,7 +252,7 @@ $(document).ready(function () {
   }
 
   $("a[data-editor=remote]").click(function (e) {
-    var params = OSM.mapParams(this.search);
+    const params = OSM.mapParams(this.search);
     remoteEditHandler(map.getBounds(), params.object);
     e.preventDefault();
   });
@@ -274,7 +272,7 @@ $(document).ready(function () {
   }
 
   OSM.Index = function (map) {
-    var page = {};
+    const page = {};
 
     page.pushstate = page.popstate = function () {
       map.setSidebarOverlaid(true);
@@ -296,7 +294,7 @@ $(document).ready(function () {
   };
 
   OSM.Browse = function (map, type) {
-    var page = {};
+    const page = {};
 
     page.pushstate = page.popstate = function (path, id) {
       OSM.loadSidebarContent(path, function () {
@@ -327,7 +325,7 @@ $(document).ready(function () {
   };
 
   OSM.OldBrowse = function () {
-    var page = {};
+    const page = {};
 
     page.pushstate = page.popstate = function (path) {
       OSM.loadSidebarContent(path);
@@ -336,7 +334,7 @@ $(document).ready(function () {
     return page;
   };
 
-  var history = OSM.History(map);
+  const history = OSM.History(map);
 
   OSM.router = OSM.Router(map, {
     "/": OSM.Index(map),
