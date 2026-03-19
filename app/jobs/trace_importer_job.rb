6138e37a8d21b362c7f420afb49d503e1b1eb040
Pablo Brasero <pablo@pablobm.com>
2026-03-19 15:23:43 +0000

    Use Noticed to deliver notifications on GPX import failure
    The previous code relied on `#deliver` (alias for `#deliver_now`) to
    immediately deliver the email so that the trace could be deleted. To
    work around this, we pass the details of the trace instead of the trace
    record, which won't be available by the time the notifications are
    ultimately delivered.
    
    This means the trace is not there to be the `record`, so we are not
    passing one. Since we can't get the recipient from the non-existing
    record, we give the recipient explicitly to `#deliver_later`.
    
    Additionally, the mailer method now needs to receive the recipient
    directly, instead of extracting it from the trace record.

--- app/jobs/trace_importer_job.rb
+++ app/jobs/trace_importer_job.rb
@@ -9,17 +9,26 @@ class TraceImporterJob < ApplicationJob
     if gpx.actual_points.positive?
       GpxImportSuccessNotifier.with(:record => trace, :possible_points => gpx.actual_points).deliver_later
     else
-      UserMailer.with(:trace => trace, :error => "0 points parsed ok. Do they all have lat,lng,alt,timestamp?").gpx_failure.deliver
-      trace.destroy
+      handle_import_failure_notification(trace, "0 points parsed ok. Do they all have lat,lng,alt,timestamp?")
     end
   rescue LibXML::XML::Error => e
     logger.info e.to_s
-    UserMailer.with(:trace => trace, :error => e).gpx_failure.deliver
-    trace.destroy
+    handle_import_failure_notification(trace, e.to_s)
   rescue StandardError => e
     logger.info e.to_s
     e.backtrace.each { |l| logger.info l }
-    UserMailer.with(:trace => trace, :error => "#{e}\n#{e.backtrace.join("\n")}").gpx_failure.deliver
+    handle_import_failure_notification(trace, "#{e}\n#{e.backtrace.join("\n")}")
+  end
+
+  private
+
+  def handle_import_failure_notification(trace, error)
+    GpxImportFailureNotifier.with(
+      :trace_name => trace.name,
+      :trace_description => trace.description,
+      :trace_tags => trace.tags.map(&:tag),
+      :error => error
+    ).deliver_later(trace.user)
     trace.destroy
   end
 end
